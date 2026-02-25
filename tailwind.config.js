9ead610f11424914562cc57c958c2e99619b84c1
mehmetbasargan <mehmet.basargan@gmail.com>
2026-02-25 11:11:02 +0300

    tailwind v3

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+		'./layout/*.liquid',
+		'./sections/*.liquid',
+		'./snippets/*.liquid',
+		'./templates/*.liquid',
+		'./config/*.json',
+	],
+	theme: {
+		extend: {
+			colors: {
+				primary: '#3b443c',
+				secondary: '#b2ac88',
+				'brand-bg': '#f9f9f9',
+			},
+			fontFamily: {
+				inter: ['Inter', 'sans-serif'],
+				josefin: ['Josefin Sans', 'sans-serif'],
+			},
+			// Container'ı burada özelleştiriyoruz
+			container: {
+				center: true,
+				padding: '1rem',
+				screens: {
+					sm: '100%',
+					md: '100%',
+					lg: '1280px',
+				},
+			},
+		},
+	},
+	plugins: [],
+};
